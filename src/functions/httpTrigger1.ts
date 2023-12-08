57617ac5985aab5bb7e6c2f24a4a21b3aa8935d4
szlfwolf <szlfwolf@outlook.com>
2023-12-09 00:05:11 +0800

    init

---
+++ src/functions/httpTrigger1.ts
@@ -0,0 +1,17 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function langOptions(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    return { 
+        body: `[{"code":"eng","name":"English"},{"code":"fre","name":"French"},{"code":"jp","name":"Japanese"}]`,
+        headers: {'Content-Type': 'application/json'}
+    };
+};
+
+app.http('langOptions', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: langOptions
+
+});
